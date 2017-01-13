c0d9efa84f6ac0b14734cf87052888c5ababb0f6
Kyle Wilcox <kyle@axiomdatascience.com>
2017-01-13 12:06:16 -0500

    Move everything to numpy and prettify it

--- pygc/gc.py
+++ pygc/gc.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 
 
@@ -108,7 +107,7 @@ def great_distance(**kwargs):
 # | forward and reverse azimuths between the points (alpha12, alpha21). |
 # |                                                                     |
 # -----------------------------------------------------------------------
-def vinc_dist(  f,  a,  phi1,  lembda1,  phi2,  lembda2 ) :
+def vinc_dist(f, a, phi1, lembda1, phi2, lembda2):
     """
 
     Returns the distance between two geographic points on the ellipsoid
@@ -119,18 +118,18 @@ def vinc_dist(  f,  a,  phi1,  lembda1,  phi2,  lembda2 ) :
 
     """
 
-    if (abs( phi2 - phi1 ) < 1e-8) and ( abs( lembda2 - lembda1) < 1e-8 ) :
-      return 0.0, 0.0, 0.0
+    if (np.absolute(phi2 - phi1) < 1e-8) and (np.absolute(lembda2 - lembda1) < 1e-8):
+        return 0.0, 0.0, 0.0
 
-    two_pi = 2.0*math.pi
+    two_pi = 2.0 * np.pi
 
     b = a * (1.0 - f)
 
-    TanU1 = (1-f) * math.tan( phi1 )
-    TanU2 = (1-f) * math.tan( phi2 )
+    TanU1 = (1 - f) * np.tan(phi1)
+    TanU2 = (1 - f) * np.tan(phi2)
 
-    U1 = math.atan(TanU1)
-    U2 = math.atan(TanU2)
+    U1 = np.arctan(TanU1)
+    U2 = np.arctan(TanU2)
 
     lembda = lembda2 - lembda1
     last_lembda = -4000000.0                # an impossibe value
@@ -139,79 +138,81 @@ def vinc_dist(  f,  a,  phi1,  lembda1,  phi2,  lembda2 ) :
     # Iterate the following equations,
     #  until there is no significant change in lembda
 
-    while ( last_lembda < -3000000.0 or lembda != 0 and abs( (last_lembda - lembda)/lembda) > 1.0e-9 ) :
+    while (last_lembda < -3000000.0 or lembda != 0 and np.absolute((last_lembda - lembda) / lembda) > 1.0e-9):
 
-      sqr_sin_sigma = pow( math.cos(U2) * math.sin(lembda), 2) + \
-            pow( (math.cos(U1) * math.sin(U2) - \
-            math.sin(U1) *  math.cos(U2) * math.cos(lembda) ), 2 )
+        sqr_sin_sigma = np.power( np.cos(U2) * np.sin(lembda), 2) + \
+            np.power((np.cos(U1) * np.sin(U2) -
+                      np.sin(U1) * np.cos(U2) * np.cos(lembda)), 2)
 
-      Sin_sigma = math.sqrt( sqr_sin_sigma )
+        Sin_sigma = np.sqrt(sqr_sin_sigma)
 
-      Cos_sigma = math.sin(U1) * math.sin(U2) + math.cos(U1) * math.cos(U2) * math.cos(lembda)
+        Cos_sigma = np.sin(U1) * np.sin(U2) + np.cos(U1) * \
+            np.cos(U2) * np.cos(lembda)
 
-      sigma = math.atan2( Sin_sigma, Cos_sigma )
+        sigma = np.arctan2(Sin_sigma, Cos_sigma)
 
-      Sin_alpha = math.cos(U1) * math.cos(U2) * math.sin(lembda) / math.sin(sigma)
-      if Sin_alpha > 1 and np.allclose(Sin_alpha, 1.0):
-        Sin_alpha = 1.0
-      elif Sin_alpha < -1 and np.allclose(Sin_alpha, -1.0):
-        Sin_alpha = -1.0
-      alpha = math.asin( Sin_alpha )
+        Sin_alpha = np.cos(U1) * np.cos(U2) * np.sin(lembda) / np.sin(sigma)
+        if Sin_alpha > 1 and np.allclose(Sin_alpha, 1.0):
+            Sin_alpha = 1.0
+        elif Sin_alpha < -1 and np.allclose(Sin_alpha, -1.0):
+            Sin_alpha = -1.0
+        alpha = np.arcsin(Sin_alpha)
 
-      Cos2sigma_m = math.cos(sigma) - (2 * math.sin(U1) * math.sin(U2) / pow(math.cos(alpha), 2) )
+        Cos2sigma_m = np.cos(sigma) - (2 * np.sin(U1) *
+                                       np.sin(U2) / np.power(np.cos(alpha), 2))
 
-      C = (f/16) * pow(math.cos(alpha), 2) * (4 + f * (4 - 3 * pow(math.cos(alpha), 2)))
+        C = (f / 16) * np.power(np.cos(alpha), 2) * \
+            (4 + f * (4 - 3 * np.power(np.cos(alpha), 2)))
 
-      last_lembda = lembda
+        last_lembda = lembda
 
-      lembda = omega + (1-C) * f * math.sin(alpha) * (sigma + C * math.sin(sigma) * \
-            (Cos2sigma_m + C * math.cos(sigma) * (-1 + 2 * pow(Cos2sigma_m, 2) )))
+        lembda = omega + (1 - C) * f * np.sin(alpha) * (sigma + C * np.sin(sigma) *
+                                                        (Cos2sigma_m + C * np.cos(sigma) * (-1 + 2 * np.power(Cos2sigma_m, 2))))
 
+    u2 = np.power(np.cos(alpha), 2) * (a * a - b * b) / (b * b)
 
-    u2 = pow(math.cos(alpha),2) * (a*a-b*b) / (b*b)
+    A = 1 + (u2 / 16384) * (4096 + u2 * (-768 + u2 * (320 - 175 * u2)))
 
-    A = 1 + (u2/16384) * (4096 + u2 * (-768 + u2 * (320 - 175 * u2)))
+    B = (u2 / 1024) * (256 + u2 * (-128 + u2 * (74 - 47 * u2)))
 
-    B = (u2/1024) * (256 + u2 * (-128+ u2 * (74 - 47 * u2)))
-
-    delta_sigma = B * Sin_sigma * (Cos2sigma_m + (B/4) * \
-            (Cos_sigma * (-1 + 2 * pow(Cos2sigma_m, 2) ) - \
-            (B/6) * Cos2sigma_m * (-3 + 4 * sqr_sin_sigma) * \
-            (-3 + 4 * pow(Cos2sigma_m,2 ) )))
+    delta_sigma = B * Sin_sigma * (Cos2sigma_m + (B / 4) *
+                                   (Cos_sigma * (-1 + 2 * np.power(Cos2sigma_m, 2)) -
+                                    (B / 6) * Cos2sigma_m * (-3 + 4 * sqr_sin_sigma) *
+                                    (-3 + 4 * np.power(Cos2sigma_m, 2))))
 
     s = b * A * (sigma - delta_sigma)
 
-    alpha12 = math.atan2( (math.cos(U2) * math.sin(lembda)), \
-            (math.cos(U1) * math.sin(U2) - math.sin(U1) * math.cos(U2) * math.cos(lembda)))
+    alpha12 = np.arctan2((np.cos(U2) * np.sin(lembda)),
+                         (np.cos(U1) * np.sin(U2) - np.sin(U1) * np.cos(U2) * np.cos(lembda)))
 
-    alpha21 = math.atan2( (math.cos(U1) * math.sin(lembda)), \
-            (-math.sin(U1) * math.cos(U2) + math.cos(U1) * math.sin(U2) * math.cos(lembda)))
+    alpha21 = np.arctan2((np.cos(U1) * np.sin(lembda)),
+                         (-np.sin(U1) * np.cos(U2) + np.cos(U1) * np.sin(U2) * np.cos(lembda)))
 
-    if ( alpha12 < 0.0 ) :
-            alpha12 =  alpha12 + two_pi
-    if ( alpha12 > two_pi ) :
-            alpha12 = alpha12 - two_pi
+    if (alpha12 < 0.0):
+        alpha12 = alpha12 + two_pi
+    if (alpha12 > two_pi):
+        alpha12 = alpha12 - two_pi
 
     alpha21 = alpha21 + two_pi / 2.0
-    if ( alpha21 < 0.0 ) :
-            alpha21 = alpha21 + two_pi
-    if ( alpha21 > two_pi ) :
-            alpha21 = alpha21 - two_pi
+    if (alpha21 < 0.0):
+        alpha21 = alpha21 + two_pi
+    if (alpha21 > two_pi):
+        alpha21 = alpha21 - two_pi
 
-    return s, alpha12,  alpha21
+    return s, alpha12, alpha21
 
 
-#----------------------------------------------------------------------------
-# Vincenty's Direct formulae                                                |
-# Given: latitude and longitude of a point (phi1, lembda1) and              |
-# the geodetic azimuth (alpha12)                                            |
-# and ellipsoidal distance in metres (s) to a second point,                 |
-#                                                                           |
-# Calculate: the latitude and longitude of the second point (phi2, lembda2) |
-# and the reverse azimuth (alpha21).                                        |
-#                                                                           |
-#----------------------------------------------------------------------------
-def vinc_pt( f, a, phi1, lembda1, alpha12, s ) :
+# ----------------------------------------------------------------------------
+# | Vincenty's Direct formulae                                               |
+# | Given: latitude and longitude of a point (phi1, lembda1) and             |
+# | the geodetic azimuth (alpha12)                                           |
+# | and ellipsoidal distance in metres (s) to a second point,                |
+# |                                                                          |
+# | Calculate: the latitude and longitude of the second point (phi2, lembda2)|
+# | and the reverse azimuth (alpha21).                                       |
+# |                                                                          |
+# ----------------------------------------------------------------------------
+def vinc_pt(f, a, phi1, lembda1, alpha12, s):
     """
 
     Returns: lat and long of projected point and reverse azimuth,
@@ -222,26 +223,25 @@ def vinc_pt( f, a, phi1, lembda1, alpha12, s ) :
 
     """
 
-    two_pi = 2.0*math.pi
+    two_pi = 2.0 * np.pi
 
-    if ( alpha12 < 0.0 ) :
-            alpha12 = alpha12 + two_pi
-    if ( alpha12 > two_pi ) :
-            alpha12 = alpha12 - two_pi
-
+    if (alpha12 < 0.0):
+        alpha12 = alpha12 + two_pi
+    if (alpha12 > two_pi):
+        alpha12 = alpha12 - two_pi
 
     b = a * (1.0 - f)
 
-    TanU1 = (1-f) * math.tan(phi1)
-    U1 = math.atan( TanU1 )
-    sigma1 = math.atan2( TanU1, math.cos(alpha12) )
-    Sinalpha = math.cos(U1) * math.sin(alpha12)
+    TanU1 = (1 - f) * np.tan(phi1)
+    U1 = np.arctan(TanU1)
+    sigma1 = np.arctan2(TanU1, np.cos(alpha12))
+    Sinalpha = np.cos(U1) * np.sin(alpha12)
     cosalpha_sq = 1.0 - Sinalpha * Sinalpha
 
-    u2 = cosalpha_sq * (a * a - b * b ) / (b * b)
-    A = 1.0 + (u2 / 16384) * (4096 + u2 * (-768 + u2 * \
-            (320 - 175 * u2) ) )
-    B = (u2 / 1024) * (256 + u2 * (-128 + u2 * (74 - 47 * u2) ) )
+    u2 = cosalpha_sq * (a * a - b * b) / (b * b)
+    A = 1.0 + (u2 / 16384) * (4096 + u2 * (-768 + u2 *
+                                           (320 - 175 * u2)))
+    B = (u2 / 1024) * (256 + u2 * (-128 + u2 * (74 - 47 * u2)))
 
     # Starting with the approximation
     sigma = (s / (b * A))
@@ -257,45 +257,42 @@ def vinc_pt( f, a, phi1, lembda1, alpha12, s ) :
 
     # two_sigma_m , delta_sigma
 
-    while ( abs( (last_sigma - sigma) / sigma) > 1.0e-9 ) :
+    while (abs((last_sigma - sigma) / sigma) > 1.0e-9):
 
-       two_sigma_m = 2 * sigma1 + sigma
+        two_sigma_m = 2 * sigma1 + sigma
 
-       delta_sigma = B * math.sin(sigma) * ( math.cos(two_sigma_m) \
-                    + (B/4) * (math.cos(sigma) * \
-                    (-1 + 2 * math.pow( math.cos(two_sigma_m), 2 ) -  \
-                    (B/6) * math.cos(two_sigma_m) * \
-                    (-3 + 4 * math.pow(math.sin(sigma), 2 )) *  \
-                    (-3 + 4 * math.pow( math.cos (two_sigma_m), 2 ))))) \
+        delta_sigma = B * np.sin(sigma) * (np.cos(two_sigma_m) +
+                                           (B / 4) * (np.cos(sigma) *
+                                                      (-1 + 2 * np.power(np.cos(two_sigma_m), 2) -
+                                                       (B / 6) * np.cos(two_sigma_m) *
+                                                       (-3 + 4 * np.power(np.sin(sigma), 2)) *
+                                                       (-3 + 4 * np.power( np.cos(two_sigma_m), 2 ))))) \
 
-       last_sigma = sigma
-       sigma = (s / (b * A)) + delta_sigma
+        last_sigma = sigma
+        sigma = (s / (b * A)) + delta_sigma
 
+    phi2 = np.arctan2((np.sin(U1) * np.cos(sigma) + np.cos(U1) * np.sin(sigma) * np.cos(alpha12)),
+                      ((1 - f) * np.sqrt(np.power(Sinalpha, 2) +
+                                         np.power(np.sin(U1) * np.sin(sigma) - np.cos(U1) * np.cos(sigma) * np.cos(alpha12), 2))))
 
-    phi2 = math.atan2 ( (math.sin(U1) * math.cos(sigma) + math.cos(U1) * math.sin(sigma) * math.cos(alpha12) ), \
-            ((1-f) * math.sqrt( math.pow(Sinalpha, 2) +  \
-            pow(math.sin(U1) * math.sin(sigma) - math.cos(U1) * math.cos(sigma) * math.cos(alpha12), 2))))
+    lembda = np.arctan2((np.sin(sigma) * np.sin(alpha12)), (np.cos(U1) * np.cos(sigma) -
+                                                            np.sin(U1) * np.sin(sigma) * np.cos(alpha12)))
 
+    C = (f / 16) * cosalpha_sq * (4 + f * (4 - 3 * cosalpha_sq))
 
-    lembda = math.atan2( (math.sin(sigma) * math.sin(alpha12 )), (math.cos(U1) * math.cos(sigma) -  \
-            math.sin(U1) *  math.sin(sigma) * math.cos(alpha12)))
-
-    C = (f/16) * cosalpha_sq * (4 + f * (4 - 3 * cosalpha_sq ))
-
-    omega = lembda - (1-C) * f * Sinalpha *  \
-            (sigma + C * math.sin(sigma) * (math.cos(two_sigma_m) + \
-            C * math.cos(sigma) * (-1 + 2 * math.pow(math.cos(two_sigma_m),2) )))
+    omega = lembda - (1 - C) * f * Sinalpha *  \
+        (sigma + C * np.sin(sigma) * (np.cos(two_sigma_m) +
+                                      C * np.cos(sigma) * (-1 + 2 * np.power(np.cos(two_sigma_m), 2))))
 
     lembda2 = lembda1 + omega
 
-    alpha21 = math.atan2 ( Sinalpha, (-math.sin(U1) * math.sin(sigma) +  \
-            math.cos(U1) * math.cos(sigma) * math.cos(alpha12)))
+    alpha21 = np.arctan2(Sinalpha, (-np.sin(U1) * np.sin(sigma) +
+                                    np.cos(U1) * np.cos(sigma) * np.cos(alpha12)))
 
     alpha21 = alpha21 + two_pi / 2.0
-    if ( alpha21 < 0.0 ) :
-            alpha21 = alpha21 + two_pi
-    if ( alpha21 > two_pi ) :
-            alpha21 = alpha21 - two_pi
+    if (alpha21 < 0.0):
+        alpha21 = alpha21 + two_pi
+    if (alpha21 > two_pi):
+        alpha21 = alpha21 - two_pi
 
-
-    return phi2,  lembda2,  alpha21
+    return phi2, lembda2, alpha21
