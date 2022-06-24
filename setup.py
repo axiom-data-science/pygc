31f0d342f3f9e7a160c68684cbcafc76a2118073
Filipe Fernandes <ocefpaf@gmail.com>
2022-06-24 16:11:15 -0300

    modern python packaging

--- setup.py
+++ setup.py
@@ -1,39 +1,10 @@
-from __future__ import with_statement
-from setuptools import setup, find_packages
-
-
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
-
-def version():
-    with open('VERSION') as f:
-        return f.read().strip()
-
-
-reqs = [line.strip() for line in open('requirements.txt')]
+from setuptools import setup
 
 setup(
-    namespace_packages = [],
-    name                 = "pygc",
-    version              = version(),
-    description          = "Great Circle calculations in Python using Vincenty's formulae",
-    long_description     = readme(),
-    long_description_content_type='text/markdown',
-    license              = 'MIT',
-    author               = "Kyle Wilcox",
-    author_email         = "kyle@axiomdatascience.com",
-    url                  = "https://github.com/axiom-data-science/pygc",
-    packages             = find_packages(),
-    install_requires     = reqs,
-    classifiers          = [
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Scientific/Engineering',
-    ],
+    name="pygc",
+    use_scm_version={
+        "write_to": "pygc/_version.py",
+        "write_to_template": '__version__ = "{version}"',
+        "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
+    },
 )
